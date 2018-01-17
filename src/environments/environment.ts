67ac3b646facd631fa6e1491c83953c1b033c7a7
Ken Skversky <ken.skversky@gmail.com>
2018-01-17 12:55:24 -0500

    Initial Angular Setup and Firebase Config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDzs4zHYBLowdwT5MLc2I3aeRH5tEt8VaY",
+    authDomain: "angular5crud-8aaa1.firebaseapp.com",
+    databaseURL: "https://angular5crud-8aaa1.firebaseio.com",
+    projectId: "angular5crud-8aaa1",
+    storageBucket: "angular5crud-8aaa1.appspot.com",
+    messagingSenderId: "358588927104"
+  }
 };
